28dfc913e847006e98a430f6ebdb10f834f18bf7
mtrella <89224976+mtrella@users.noreply.github.com>
2021-10-07 20:47:18 -0500

    Add files via upload

---
+++ MovieLibrary/ShowLibrary.cs
@@ -0,0 +1,103 @@
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShowLibrary
+{
+    public class ShowFile
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // public property
+        public string filePath { get; set; }
+        public List<Show> Shows { get; set; }
+
+        public ShowFile(string path)
+        {
+            Shows = new List<Show>();
+            filePath = path;
+            // read data from file
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    Show show = new Show();
+                    string line = sr.ReadLine();
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        // no quote = no comma in movie title
+                        // movie details are separated with ,
+                        string[] showDetails = line.Split(',');
+                        show.showId = UInt64.Parse(showDetails[0]);
+                        show.title = showDetails[1];
+                        show.season = showDetails[2];
+                        show.episode = showDetails[3];
+                        show.writers = showDetails[4].Split('|').ToList();
+                    }
+                    else
+                    {
+                        // quote = comma in show title
+                        // extract the showId
+                        show.showId = UInt64.Parse(line.Substring(0, idx - 1));
+                        // remove showId and first quote from string
+                        line = line.Substring(idx + 1);
+                        // find the next quote
+                        idx = line.IndexOf('"');
+                        // extract showTitle
+                        show.title = line.Substring(0, idx);
+                        // remove title and last comma from the string
+                        line = line.Substring(idx + 2);
+                        // replace the "|" with ", "
+                        show.writers = line.Split('|').ToList();
+                    }
+                    Shows.Add(show);
+                }
+                // close file when finished
+                sr.Close();
+                logger.Info("Movies in file {Count}", Shows.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+
+        // public method
+        public bool isUniqueTitle(string title)
+        {
+            if (Shows.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
+            {
+                logger.Info("Duplicate show title {Title}", title);
+                return false;
+            }
+            return true;
+        }
+
+        public void AddShow(Show show)
+        {
+            try
+            {
+                // first generate movie id
+                show.showId = Shows.Max(m => m.movieId) + 1;
+                // if title contains a comma, wrap it in quotes
+                string title = show.title.IndexOf(',') != -1 ? $"\"{show.title}\"" : show.title;
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine($"{show.showId},{title},{string.Join("|", show.writers)}");
+                sw.Close();
+                // add movie details to Lists
+                Shows.Add(show);
+                // log transaction
+                logger.Info("Show id {Id} added", show.showId);
+            } 
+            catch(Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+    }
+}
---
+++ MovieLibrary/VideoLibrary.cs
@@ -0,0 +1,103 @@
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShowLibrary
+{
+    public class VideoFile
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // public property
+        public string filePath { get; set; }
+        public List<Video> Videos { get; set; }
+
+        public VideoFile(string path)
+        {
+            Videos = new List<Video>();
+            filePath = path;
+            // read data from file
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    Video video = new Video();
+                    string line = sr.ReadLine();
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        // no quote = no comma in video title
+                        // video details are separated with ,
+                        string[] videoDetails = line.Split(',');
+                        video.videoId = UInt64.Parse(videoDetails[0]);
+                        video.title = videoDetails[1];
+                        video.format = videoDetails[2];
+                        video.length = videoDetails[3];
+                        video.regions = videoDetails[4].Split('|').ToList();
+                    }
+                    else
+                    {
+                        // quote = comma in show title
+                        // extract the showId
+                        video.videoId = UInt64.Parse(line.Substring(0, idx - 1));
+                        // remove showId and first quote from string
+                        line = line.Substring(idx + 1);
+                        // find the next quote
+                        idx = line.IndexOf('"');
+                        // extract showTitle
+                        video.title = line.Substring(0, idx);
+                        // remove title and last comma from the string
+                        line = line.Substring(idx + 2);
+                        // replace the "|" with ", "
+                        video.writers = line.Split('|').ToList();
+                    }
+                    Videos.Add(video);
+                }
+                // close file when finished
+                sr.Close();
+                logger.Info("Videos in file {Count}", Videos.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+
+        // public method
+        public bool isUniqueTitle(string title)
+        {
+            if (Videos.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
+            {
+                logger.Info("Duplicate show title {Title}", title);
+                return false;
+            }
+            return true;
+        }
+
+        public void AddVideo(Video video)
+        {
+            try
+            {
+                // first generate movie id
+                video.videoId = Videos.Max(m => m.movieId) + 1;
+                // if title contains a comma, wrap it in quotes
+                string title = video.title.IndexOf(',') != -1 ? $"\"{video.title}\"" : video.title;
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine($"{video.videoId},{title},{string.Join("|", video.regions)}");
+                sw.Close();
+                // add movie details to Lists
+                Videos.Add(video);
+                // log transaction
+                logger.Info("Video id {Id} added", video.videoId);
+            } 
+            catch(Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+    }
+}
